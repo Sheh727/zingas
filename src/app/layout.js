9aa5420178b311663388a4ea9d7d8fd54d82fcb2
Sheheryar Tahir <sono848382@gmail.com>
2025-09-13 16:20:36 +0500

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,12 +25,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="smooth-scroll">
       <body 
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden `}
       >
         
         <MotionConfig viewport={{once: true}}>
         {children}
-       
+        
         </MotionConfig>
       
       </body>
